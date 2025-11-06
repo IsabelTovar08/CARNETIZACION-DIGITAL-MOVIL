137fba959816e2bcfa5150fcae3ea27aa1061235
Isabel Tovar <Isabeltovarp.18@gmail.com>
2025-11-06 00:09:01 -0500

    .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,8 @@
 export const ENV = {
   // API_BASE: 'http://172.30.3.251:8008/api', 
   // API_BASE: 'http://10.3.235.164:5100/api', 
-  API_BASE: 'http://192.168.137.1:5100/api', 
-
+  // API_BASE: 'http://192.168.137.1:5100/api', 
+  API_BASE: 'https://carnetizacion-api-h5d3gcg7a0c9ffek.centralus-01.azurewebsites.net/api', 
 
   DEBUG_HTTP: true,
   TOKEN_ENDPOINTS: {
